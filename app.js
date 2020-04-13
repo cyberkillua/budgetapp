7c6e582a892b5276f7ee506e901fe9369924e1c4
Moshood Alimi <moshood988@gmail.com>
2020-04-13 01:17:38 +0100

    new commit for JS

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var budgetController = (function () {
   var Income = function (id, description, value) {
     this.id = id;
-    this.description = id;
+    this.description = description;
     this.value = value;
   };
 
   var Expenses = function (id, description, value) {
     this.id = id;
-    this.description = id;
+    this.description = description;
     this.value = value;
   };
 
@@ -21,22 +21,41 @@ var budgetController = (function () {
       exp: 0,
     },
   };
+
+  return {
+    additem: function (type, des, val) {
+      var ID;
+      if (data.allitems[type].length > 0) {
+        data.allitems[type][data.allitems[type].length - 1].id - 1;
+      } else {
+        ID = 0;
+      }
+
+      if (type === "exp") {
+        var newItem = new Expenses(ID, des, val);
+      } else if (type === "inc") {
+        var newItem = new Income(ID, des, val);
+      }
+      data.allitems[type].push(newItem);
+      return newItem;
+    },
+  };
 })();
 
 var UIController = (function () {
   var DOMStrings = {
-    inputText: ".add__description",
-    inputType: ".add__type",
-    inputNumber: ".add__value",
+    inputdescription: ".add__description",
+    inputtype: ".add__type",
+    inputvalue: ".add__value",
     inputBtn: ".add__btn",
   };
 
   return {
     getInput: function () {
       return {
-        Text: document.querySelector(DOMStrings.inputText).value,
-        Type: document.querySelector(DOMStrings.inputType).value,
-        Number: document.querySelector(DOMStrings.inputNumber).value,
+        type: document.querySelector(DOMStrings.inputtype).value,
+        description: document.querySelector(DOMStrings.inputdescription).value,
+        value: document.querySelector(DOMStrings.inputvalue).value,
       };
     },
 
@@ -65,6 +84,11 @@ var appController = (function (budgetCtrl, UICtrl) {
     console.log(input);
 
     // add the item to the budget contoller
+    var addStuff = budgetCtrl.additem(
+      input.type,
+      input.description,
+      input.value
+    );
 
     // display the item in the UI
 
